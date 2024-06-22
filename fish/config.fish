2a0d86820ca8167578ad6a5a610c090eb58a8c60
Razvan Muntian <144447396+razvanmtn@users.noreply.github.com>
2024-06-22 12:57:06 +0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 source (dirname (status --current-filename))/alias.fish
 
+set --universal tide_left_prompt_items pwd node
+set --universal tide_right_prompt_items node time
+
 set --universal nvm_default_version v18.20.3
