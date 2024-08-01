0ac13d3fc7d894daaca8cc314f38f7a34009584f
Razvan Muntian <144447396+razvanmtn@users.noreply.github.com>
2024-08-01 14:28:34 +0300

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,8 @@ return {
     {
         'neovim/nvim-lspconfig',
         dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
             {
                 'williamboman/nvim-lsp-installer',
                 config = function()
@@ -10,6 +12,22 @@ return {
             },
         },
         config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "tsserver",
+                    "dockerls",
+                    "cssls",
+                    "html",
+                    "eslint",
+                    "cssls",
+                    "yamlls",
+                    "jsonls",
+                    "gopls",
+                    "rust_analyzer",
+                },
+            })
+
             local lspConfig = require("lspconfig");
             
             lspConfig.tsserver.setup{}
@@ -21,6 +39,30 @@ return {
             lspConfig.yamlls.setup{}
             lspConfig.jsonls.setup{}
             lspConfig.sqlls.setup{}
+            lspConfig.gopls.setup{}
+            lspConfig.rust_analyzer.setup{
+                on_attach=on_attach,
+                settings = {
+                    ['rust-analyzer'] = {
+                        diagnostics = {
+                            enable = true;
+                        },
+                        imports = {
+                            granularity = {
+                                group = "module",
+                            },
+                            prefix = "self",
+                        },
+                        cargo = {
+                            buildScripts = {
+                                enable = true,
+                            },
+                        },
+                        procMacro = {
+                            enable = true
+                        }
+                    }
+                }
         end,
     },
 }
