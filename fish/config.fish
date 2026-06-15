d7e1ea428b4a2efe852f4dc7f152c44eacfd6ee6
Razvan Muntian <razvanmuntian@gmail.com>
2026-06-15 20:44:43 +0300

    feat: update nvm default version and remove fish greetings message

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ source (dirname (status --current-filename))/alias.fish
 set --universal tide_left_prompt_items pwd node
 set --universal tide_right_prompt_items node time
 
-set --universal nvm_default_version v18.20.3
+set --universal nvm_default_version v22.18.0
+
+set --universal fish_greeting
 
 fish_add_path $HOME/go/bin
